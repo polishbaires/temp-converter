d5fc34f345070ae4aa44dd6eca65b9de9490a6d0
Luis <luis.pulido.d@bairesdev.com>
2021-10-04 16:31:21 -0700

    Improved precision

--- c_to_f.rb
+++ c_to_f.rb
@@ -1,6 +1,6 @@
 # celcius to farenheit
 def convert (temp)
-  ( temp * 1.8 ) + 32
+  ( temp * (9/5) ) + 32
 end
 
 puts convert ARGV.first.to_f
--- f_to_c.rb
+++ f_to_c.rb
@@ -1,6 +1,6 @@
 # farenheit to celcius
 def convert (temp)
-  ( temp - 32 ) * 0.55
+  ( temp - 32.0 ) * 5/9
 end
 
 puts convert ARGV.first.to_f
